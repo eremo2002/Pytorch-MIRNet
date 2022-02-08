fcd156208f3a0610f945b2d936e7c5413f2fb732
eremo2002 <33386742+eremo2002@users.noreply.github.com>
2022-02-08 23:32:41 +0900

    Create loss.py

---
+++ util/loss.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class L1_Loss(nn.Module):
+    def __init__(self):
+        super(L1_Loss, self).__init__()
+
+    def forward(self, x, y):
+        loss = F.l1_loss(x, y, reduction='mean')        
+        return loss * 1000
+
+class Smooth_L1_Loss(nn.Module):
+    def __init__(self):
+        super(Smooth_L1_Loss, self).__init__()
+
+    def forward(self, x, y):
+        loss = F.smooth_l1_loss(x, y, reduction='mean')        
+        return loss * 1000
+
+
+class L1_L2_Loss(nn.Module):
+    def __init__(self, ratio):
+        super(L1_L2_Loss, self).__init__()
+        self.ratio = ratio
+
+    def forward(self, x, y):
+        L1_loss = F.l1_loss(x, y, reduction='mean')
+        L2_loss = F.mse_loss(x, y, reduction='mean')
+
+        L1_L2 = (self.ratio)*L1_loss + (1-self.ratio)*L2_loss
+        
+        return L1_L2
+
+class Charbonnier_Loss(nn.Module):
+    def __init__(self, eps=1e-3):
+        super(Charbonnier_Loss, self).__init__()
+        self.eps = eps
+
+    def forward(self, x, y):
+        loss = torch.mean(torch.sqrt((x-y)**2 + (self.eps)**2))
+        return loss
+
